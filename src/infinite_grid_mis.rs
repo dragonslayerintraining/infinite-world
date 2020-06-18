552301af88945adebb6859f2d0d40bec836af1bb
dragonslayerintraining <22734948+dragonslayerintraining@users.noreply.github.com>
2020-06-18 16:34:08 -0700

    Moved MIS code to separate file

---
+++ src/infinite_grid_mis.rs
@@ -0,0 +1,82 @@
+use std::cmp::max;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+
+
+fn get_cell_seq(x: i32, y: i32,t: u32) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    x.hash(&mut hasher);
+    y.hash(&mut hasher);
+    t.hash(&mut hasher);
+    return hasher.finish();
+}
+
+#[derive(Clone)]
+#[derive(Copy)]
+enum CellState{
+    Waiting,
+    Accept,
+    Reject,
+}
+
+fn get_cell_at_memo(cache: &mut HashMap<(i32,i32,u32),CellState>, x: i32, y: i32, t: u32) -> CellState {
+    fn memoize_val(cache: &mut HashMap<(i32,i32,u32),CellState>, x: i32, y: i32, t: u32, val: CellState) -> CellState {
+        cache.insert((x,y,t),val);
+        return val;
+    }
+    if let Some(state)=cache.get(&(x,y,t)){
+        return state.clone();
+    }
+    if t == 0 {
+        return CellState::Waiting;
+    }
+    match get_cell_at_memo(cache,x,y,t-1) {
+        CellState::Waiting => {},
+        CellState::Accept => return memoize_val(cache,x,y,t,CellState::Accept),
+        CellState::Reject => return memoize_val(cache,x,y,t,CellState::Reject),
+    }
+
+    let mut score: u64 = 0;
+    for dx in -1..2 {
+        for dy in -1..2 {
+            if dx==0 && dy==0 { continue; }
+            match get_cell_at_memo(cache,x+dx,y+dy,t-1) {
+                CellState::Waiting => score = max(score,get_cell_seq(x+dx,y+dy,t-1)),
+                CellState::Accept => return memoize_val(cache,x,y,t,CellState::Reject),
+                CellState::Reject => {},
+            }
+        }
+    }
+    if get_cell_seq(x,y,t-1)>score {
+        return memoize_val(cache,x,y,t,CellState::Accept);
+    } else {
+        return memoize_val(cache,x,y,t,CellState::Waiting);
+    }
+}
+
+fn get_cell_memo(cache: &mut HashMap<(i32,i32,u32),CellState>, x: i32, y: i32) -> bool {
+    let mut t = 0;
+    loop {
+        match get_cell_at_memo(cache,x,y,t){
+            CellState::Waiting => t+=1,
+            CellState::Accept => return true,
+            CellState::Reject => return false,
+        }
+    }
+}
+
+pub struct Cache{
+    cache: HashMap<(i32,i32,u32),CellState>,
+}
+
+impl Cache{
+    pub fn new() -> Cache{
+        Cache{
+            cache: HashMap::new(),
+        }
+    }
+    pub fn get(&mut self, x: i32, y: i32) -> bool {
+        get_cell_memo(&mut self.cache, x, y)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,16 @@
-use std::cmp::max;
-use std::collections::hash_map::DefaultHasher;
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
-
-
-fn get_cell_seq(x: i32, y: i32,t: u32) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    x.hash(&mut hasher);
-    y.hash(&mut hasher);
-    t.hash(&mut hasher);
-    return hasher.finish();
-}
-
-#[derive(Clone)]
-#[derive(Copy)]
-enum CellState{
-    Waiting,
-    Accept,
-    Reject,
-}
-
-
-#[derive(Clone)]
-#[derive(Copy)]
-enum CellResult{
-    Tree,
-    Empty,
-}
-
-fn get_cell_at_memo(cache: &mut HashMap<(i32,i32,u32),CellState>, x: i32, y: i32, t: u32) -> CellState {
-    fn memoize_val(cache: &mut HashMap<(i32,i32,u32),CellState>, x: i32, y: i32, t: u32, val: CellState) -> CellState {
-        cache.insert((x,y,t),val);
-        return val;
-    }
-    if let Some(state)=cache.get(&(x,y,t)){
-        return state.clone();
-    }
-    if t == 0 {
-        return CellState::Waiting;
-    }
-    match get_cell_at_memo(cache,x,y,t-1) {
-        CellState::Waiting => {},
-        CellState::Accept => return memoize_val(cache,x,y,t,CellState::Accept),
-        CellState::Reject => return memoize_val(cache,x,y,t,CellState::Reject),
-    }
-
-    let mut score: u64 = 0;
-    for dx in -1..2 {
-        for dy in -1..2 {
-            if dx==0 && dy==0 { continue; }
-            match get_cell_at_memo(cache,x+dx,y+dy,t-1) {
-                CellState::Waiting => score = max(score,get_cell_seq(x+dx,y+dy,t-1)),
-                CellState::Accept => return memoize_val(cache,x,y,t,CellState::Reject),
-                CellState::Reject => {},
-            }
-        }
-    }
-    if get_cell_seq(x,y,t-1)>score {
-        return memoize_val(cache,x,y,t,CellState::Accept);
-    } else {
-        return memoize_val(cache,x,y,t,CellState::Waiting);
-    }
-}
-
-fn get_cell_memo(cache: &mut HashMap<(i32,i32,u32),CellState>, x: i32, y: i32) -> CellResult {
-    let mut t = 0;
-    loop {
-        match get_cell_at_memo(cache,x,y,t){
-            CellState::Waiting => t+=1,
-            CellState::Accept => return CellResult::Tree,
-            CellState::Reject => return CellResult::Empty,
-        }
-    }
-}
+mod infinite_grid_mis;
 
 fn main() {
-    let mut cache : HashMap<(i32,i32,u32),CellState> = HashMap::new();
+    let mut cache : infinite_grid_mis::Cache = infinite_grid_mis::Cache::new();
     for x in 0..100 {
         for y in 0..100 {
-            let s: String = match get_cell_memo(&mut cache,x,y) {
-                CellResult::Tree => "\x1b[42m  \x1b[49m".to_string(),
-                CellResult::Empty => "\x1b[40m  \x1b[49m".to_string(),
+            let s: String = match cache.get(x,y) {
+                true => "\x1b[42m  \x1b[49m".to_string(),
+                false => "\x1b[40m  \x1b[49m".to_string(),
             };
             print!("{}",s);
         }
         println!();
     }
-    println!("Cache size: {}",cache.len());
+    //println!("Cache size: {}",cache.len());
 }
